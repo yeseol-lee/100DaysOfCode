ebfee48780d21927e9719e5ec27d4bc545b7d57e
yeseol-lee <1999onell@gmail.com>
2020-10-30 22:24:39 +0900

    text_analyzer_3

--- JS PROJECT/text_analyzer/main.js
+++ JS PROJECT/text_analyzer/main.js
@@ -1,16 +1,21 @@
-//95번째줄 인풋에 입력하면 스톱월드 배열로 추가하는거 하는중.
+//wordList를 글로벌 배열으로 만들어서 수정한 것이 글로벌에 저장될 수 있도록.
 
 //receive file from html and display
 document.getElementById("input-file").addEventListener("change", handleFiles);
 
 //remove selected word from the most list
-document.getElementById("most-button").addEventListener("click", rmFromList("most-input"));
+document.getElementById("most-button").addEventListener("click", function () {
+    rmFromList("most-input");
+});
 
 //remove selected word from the least list
-document.getElementById("least-button").addEventListener("click",rmFromList("least-input"));
+document.getElementById("least-button").addEventListener("click", function () {
+    rmFromList("least-input");
+});
 
 
 var stopWords = [];
+var wordList = [];
 
 function handleFiles(event) {
     let text;
@@ -50,6 +55,8 @@ function getDocStats(fileContent) {
     let wordArray = lowerText.match(/\b\S+\b/g);
     let wordDictionary = {};
 
+    //write length, wordcound, charcount
+    getBasicDocStats(fileContent, wordArray);
 
     //Count every word in the wordArray
     for (let word in wordArray) {
@@ -60,14 +67,46 @@ function getDocStats(fileContent) {
             wordDictionary[wordValue] = 1;
         }
     }
-    
+
     //sort the array
     let wordList = sortProperties(wordDictionary);
-    
-    console.log(wordList);
+
+    //get top 10 and least 5 words
+    let top10 = wordList.slice(0, 10);
+    let least10 = wordList.slice(-10, length.wordList);
+
+
+    //Write the values to the page
+    ULTemplate(top10, document.getElementById("mostUsed"));
+    ULTemplate(least10, document.getElementById("leastUsed"));
 
 
 
+}
+
+//make list template
+function ULTemplate(items, element) {
+    let rowTemplate = document.getElementById('template-ul-items');
+    let templateHTML = rowTemplate.innerHTML;
+    let resultsHTML = "";
+
+    for (i = 0; i < items.length - 1; i++) {
+        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
+    }
+
+    element.innerHTML = resultsHTML;
+
+}
+
+function getBasicDocStats(text, wordArray) {
+
+    docLength.innerText = "Document Length(including spaces) : " + text.length;
+    wordCount.innerText = "Word Count: " + wordArray.length;
+
+    //remove spaces from text string
+    text = text.replace(/ /g, "");
+
+    charCount.innerText = "Number of Characters(without spaces) : " + text.length;
 }
 
 //sort object in order of number
@@ -84,18 +123,18 @@ function sortProperties(obj) {
 }
 
 function rmFromList(inputID) {
-    
+
+    //stopWords array
     renewStopWord(inputID);
-    
+
 }
 
 
 //renew stopWord array
 function renewStopWord(inputID) {
-    
+
     let word = document.getElementById(inputID).value;
-    
-    console.log(word);
-    //stopWords.push(word);
-    //console.log(stopWords);
+    document.getElementById(inputID).value = "";
+
+    stopWords.push(word);
 }
