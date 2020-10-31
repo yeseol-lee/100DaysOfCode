783f6766625bea5d3855cfd1543f82abbf15d191
yeseol-lee <1999onell@gmail.com>
2020-10-31 11:56:11 +0900

    completed 3rd js project text_analyzer

--- JS PROJECT/text_analyzer/main.js
+++ JS PROJECT/text_analyzer/main.js
@@ -69,11 +69,11 @@ function getDocStats(fileContent) {
     }
 
     //sort the array
-    let wordList = sortProperties(wordDictionary);
+    wordList = sortProperties(wordDictionary);
 
     //get top 10 and least 5 words
-    let top10 = wordList.slice(0, 10);
-    let least10 = wordList.slice(-10, length.wordList);
+    let top10 = wordList.slice(0, 11);
+    let least10 = wordList.slice(-11, length.wordList);
 
 
     //Write the values to the page
@@ -124,17 +124,87 @@ function sortProperties(obj) {
 
 function rmFromList(inputID) {
 
-    //stopWords array
-    renewStopWord(inputID);
+    rmFromWordList(inputID);
+    rewriteList();
+
+    //remove stop Words from wordList
+    function rmFromWordList(inputID) {
+
+        let word = document.getElementById(inputID).value;
+        document.getElementById(inputID).value="";
+        //let a = 
+        //search and remove
+
+        let index;
+
+        //search index and remove from wordList.
+        for (i = 0; i < wordList.length; i++) {
+
+            if (wordList[i][0] == word) {
+
+                index = wordList.indexOf(wordList[i]);
+                wordList.splice(index, 1);
+                break;
+            }
+        }
+
+
+
+
+    }
+
+    //re-write top 10 and least 10
+    function rewriteList() {
+
+        //get top 10 and least 5 words
+        let top10 = wordList.slice(0, 11);
+        let least10 = wordList.slice(-11, length.wordList);
+
+        //Write the values to the page
+        ULTemplate(top10, document.getElementById("mostUsed"));
+        ULTemplate(least10, document.getElementById("leastUsed"));
+
+    }
 
 }
 
+//if search-button is clicked, highlight the words
+document.getElementById("search-button").addEventListener("click", performMark);
 
-//renew stopWord array
-function renewStopWord(inputID) {
-
-    let word = document.getElementById(inputID).value;
-    document.getElementById(inputID).value = "";
-
-    stopWords.push(word);
+function performMark() {
+    
+    //read the keyword
+    
+    var keyword = document.getElementById('keyword').value;
+    var display = document.getElementById("content");
+    
+    var newContent = "";
+    
+    //find all the currrently marked items
+    let spans = document.querySelectorAll("mark");
+    for (var i = 0; i< spans.length; i++) {
+        
+        spans[i].outerHTML = spans[i].innerHTML
+    }
+    
+    var re = new RegExp(keyword, "gi" );
+    var replaceText = "<mark id='markme'>$&</mark>";
+    var bookContent = display.innerHTML;
+    
+    //add the mark to the book content
+    newContent = bookContent.replace(re, replaceText);
+    
+    
+    display.innerHTML = newContent;
+    var count = document.querySelectorAll('mark').length;
+    document.getElementById("searchstat").innerHTML = "found " + count + " matches";
+    
+    if(count > 0) {
+        
+        var element = document.getElementById("markme");
+        element.scrollIntoView();
+    };
+    
+    
+    
 }
